f413a2ad284bb145b5ae9820e4f2e19aba617472
anju-kumaran <anju.naduth@gmail.com>
2021-11-01 19:24:14 -0400

    Adding an interviewers selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,3 +18,15 @@ export function getInterview(state, interview) {
   let interviewer = state.interviewers[interview.interviewer];
   return {...interview, interviewer};
 }
+
+export function getInterviewersForDay(state, day) {
+  const filteredInterviewers = [];
+  state.days.forEach(stateDay => {
+    if (stateDay.name === day) {
+      stateDay.interviewers.forEach(interviewerId => {
+        filteredInterviewers.push(state.interviewers[interviewerId])
+      })
+    }
+  })
+  return filteredInterviewers;
+}
